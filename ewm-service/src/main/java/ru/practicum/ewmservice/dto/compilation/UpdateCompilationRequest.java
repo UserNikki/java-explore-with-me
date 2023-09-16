00a5bf945cb792f548b49236069553423ec91941
Nikki <shirokovnicolas@gmail.com>
2023-09-16 21:16:50 +0300

    hell on earth

--- ewm-service/src/main/java/ru/practicum/ewmservice/dto/compilation/UpdateCompilationRequest.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/dto/compilation/UpdateCompilationRequest.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Data
