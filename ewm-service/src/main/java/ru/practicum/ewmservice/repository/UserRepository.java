3f9e1d9a53a8306f3975854849ba08e30a97149e
Nikki <shirokovnicolas@gmail.com>
2023-09-17 00:24:43 +0300

    hell on earth

--- ewm-service/src/main/java/ru/practicum/ewmservice/repository/UserRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/repository/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByIdIn(List<Long> ids, Pageable pageable);
+
     Boolean existByNameIgnoreCase(String name);
 }
