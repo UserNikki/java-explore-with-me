9e99a8ff385591273eb5e23e3c2d6f36c87f1a40
Nikki <shirokovnicolas@gmail.com>
2023-09-16 22:18:20 +0300

    hell on earth

--- ewm-service/src/main/java/ru/practicum/ewmservice/model/Category.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/model/Category.java
@@ -17,5 +17,6 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true, length = 50)
     private String name;
 }
