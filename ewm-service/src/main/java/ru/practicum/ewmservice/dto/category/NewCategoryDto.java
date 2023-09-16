462213a57283d16ba1f310c689d0c400e3a9cd71
Nikki <shirokovnicolas@gmail.com>
2023-09-16 20:32:50 +0300

    hell on earth

--- ewm-service/src/main/java/ru/practicum/ewmservice/dto/category/NewCategoryDto.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/dto/category/NewCategoryDto.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class NewCategoryDto {
+    @NotBlank
+    @Length(min = 1, max = 50)
     private String name;
 }
