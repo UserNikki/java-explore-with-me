0a3ecb79ae3f038172722f9c45712dada6eea99a
Nikki <shirokovnicolas@gmail.com>
2023-09-16 20:14:02 +0300

    hell on earth

--- ewm-service/src/main/java/ru/practicum/ewmservice/dto/event/UpdateEventUserRequest.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/dto/event/UpdateEventUserRequest.java
@@ -23,8 +23,8 @@ public class UpdateEventUserRequest {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
     private Location location;
-    private Boolean paid;
-    private Integer participantLimit;
+    private Boolean paid = false;
+    private Integer participantLimit = 0;
     private Boolean requestModeration;
     private StateActionEnum stateAction;
     @Length(min = 3, max = 120)
