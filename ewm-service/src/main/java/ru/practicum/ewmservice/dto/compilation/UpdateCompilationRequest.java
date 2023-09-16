6f8eedc7262502f87e363ec2084b9e5c5592a3a3
Nikki <shirokovnicolas@gmail.com>
2023-09-16 21:25:31 +0300

    hell on earth

--- ewm-service/src/main/java/ru/practicum/ewmservice/dto/compilation/UpdateCompilationRequest.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/dto/compilation/UpdateCompilationRequest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.List;
 
@@ -14,5 +15,6 @@ import java.util.List;
 public class UpdateCompilationRequest {
     private List<Long> events;
     private Boolean pinned;
+    @Length(min = 1, max = 50)
     private String title;
 }
